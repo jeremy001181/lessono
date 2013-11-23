19200cde248f6628f793be0d04fd7e7c2ccd7e50
Zhongming Chen <jeremy001181@gmail.com>
2013-11-23 20:14:27 +0000

    show custom error info for page doesn't exist

--- web/app.js
+++ web/app.js
@@ -14,7 +14,7 @@ var path = require('path');
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 80);
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 // app.set('settings', config);
@@ -24,8 +24,14 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
-app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public'), { maxAge: 86400000 }));
+app.use(app.router);
+
+// production
+
+if (app.configure("production", function () {
+	app.set('port', 80);
+}));
 
 // test only
 
@@ -43,6 +49,9 @@ if ('development' == app.get('env')) {
 require('./routes/results/routes')(app);
 require('./routes/home/routes')(app);
 
+app.get('*', function(req, res){
+	res.send('Say what???', 404);
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
